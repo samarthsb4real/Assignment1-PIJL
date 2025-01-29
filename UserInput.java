8c247b3e316779e5e0a5a08c137e6d8b5b036b7b
samarthsb4real <samarthbhadane23@gmail.com>
2025-01-29 22:29:34 +0530

    add constructor and method for integer input in UserInput class

--- UserInput.java
+++ UserInput.java
@@ -2,4 +2,14 @@ import java.util.Scanner;
 
 public class UserInput {
     private Scanner scanner;
+
+    public UserInput(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    public int getIntegerInput(String message) {
+        System.out.print(message);
+        return scanner.nextInt();
+    }
+
 }
