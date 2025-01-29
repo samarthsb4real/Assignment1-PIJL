70546597eee37d50bdfac63d83b2c919768c2158
samarthsb4real <samarthbhadane23@gmail.com>
2025-01-29 22:33:14 +0530

    add fibonacci method and arraySum method to Calculator class

--- Calculator.java
+++ Calculator.java
@@ -15,4 +15,24 @@ public class Calculator {
     public double divide(double a, double b) {
         return a / b;
     }
+
+    public void fibonacci(int n) {
+        int a = 0, b = 1;
+        System.out.print("Fibonacci Series: " + a + " " + b);
+        for (int i = 2; i < n; i++) {
+            int next = a + b;
+            System.out.print(" " + next);
+            a = b;
+            b = next;
+        }
+        System.out.println();
+    }
+
+    public double arraySum(double[] array) {
+        double sum = 0;
+        for (double num : array) {
+            sum += num;
+        }
+        return sum;
+    }
 }
