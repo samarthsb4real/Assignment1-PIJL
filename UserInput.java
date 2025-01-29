63b2cbf7096856c55b031e12f8ca1bffde3b6f9d
samarthsb4real <samarthbhadane23@gmail.com>
2025-01-29 22:29:56 +0530

    add method to get array input from user

--- UserInput.java
+++ UserInput.java
@@ -19,4 +19,16 @@ public class UserInput {
         double num2 = scanner.nextDouble();
         return new double[]{num1, num2};
     }
+
+    public double[] getArrayInput() {
+        System.out.print("Enter the size of the array: ");
+        int size = scanner.nextInt();
+        double[] array = new double[size];
+
+        System.out.println("Enter the elements:");
+        for (int i = 0; i < size; i++) {
+            array[i] = scanner.nextDouble();
+        }
+        return array;
+    }
 }
