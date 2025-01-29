3f59f39d89aecd48484fd0316a504e346662dbc2
samarthsb4real <samarthbhadane23@gmail.com>
2025-01-29 22:35:57 +0530

    add additional arithmetic operations and statistical methods in Main class

--- Main.java
+++ Main.java
@@ -23,6 +23,43 @@ public class Main {
                     double[] subInputs = userInput.getTwoNumbers();
                     System.out.println("Result: " + calculator.subtract(subInputs[0], subInputs[1]));
                     break;
+                case 3:
+                    double[] mulInputs = userInput.getTwoNumbers();
+                    System.out.println("Result: " + calculator.multiply(mulInputs[0], mulInputs[1]));
+                    break;
+                case 4:
+                    double[] divInputs = userInput.getTwoNumbers();
+                    if (divInputs[1] != 0) {
+                        System.out.println("Result: " + calculator.divide(divInputs[0], divInputs[1]));
+                    } else {
+                        System.out.println("Error: Division by zero.");
+                    }
+                    break;
+                case 5:
+                    int n = userInput.getIntegerInput("Enter the number of Fibonacci terms: ");
+                    calculator.fibonacci(n);
+                    break;
+                case 6:
+                    double[] array = userInput.getArrayInput();
+                    System.out.println("Sum: " + calculator.arraySum(array));
+                    break;
+                case 7:
+                    array = userInput.getArrayInput();
+                    System.out.println("Mean: " + calculator.arrayMean(array));
+                    break;
+                case 8:
+                    array = userInput.getArrayInput();
+                    System.out.println("Variance: " + calculator.arrayVariance(array));
+                    break;
+                case 9:
+                    array = userInput.getArrayInput();
+                    System.out.println("Standard Deviation: " + calculator.arrayStdDev(array));
+                    break;
+                case 10:
+                    exit = true;
+                    break;
+                default:
+                    System.out.println("Invalid choice. Try again.");
             }
         }
         scanner.close();
