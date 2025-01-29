8e73f416fea5ca4d4f69bfd4f5d7424c9515cb9c
samarthsb4real <samarthbhadane23@gmail.com>
2025-01-29 22:35:45 +0530

    add user interaction for basic arithmetic operations in Main class

--- Main.java
+++ Main.java
@@ -5,5 +5,26 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         UserInput userInput = new UserInput(scanner);
         Calculator calculator = new Calculator();
+
+        boolean exit = false;
+        while (!exit) {
+            System.out.println("\nSelect an operation:");
+            System.out.println("1. Addition\n2. Subtraction\n3. Multiplication\n4. Division\n5. Fibonacci Sequence");
+            System.out.println(
+                    "6. Sum of Array\n7. Mean of Array\n8. Variance of Array\n9. Standard Deviation\n10. Exit");
+            int choice = userInput.getIntegerInput("Enter your choice: ");
+
+            switch (choice) {
+                case 1:
+                    double[] addInputs = userInput.getTwoNumbers();
+                    System.out.println("Result: " + calculator.add(addInputs[0], addInputs[1]));
+                    break;
+                case 2:
+                    double[] subInputs = userInput.getTwoNumbers();
+                    System.out.println("Result: " + calculator.subtract(subInputs[0], subInputs[1]));
+                    break;
+            }
+        }
+        scanner.close();
     }
 }
