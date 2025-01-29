cfc0976600d022e7744a0ec301142dc55db12989
samarthsb4real <samarthbhadane23@gmail.com>
2025-01-29 22:32:26 +0530

    add multiply and divide methods to Calculator class

--- Calculator.java
+++ Calculator.java
@@ -7,4 +7,12 @@ public class Calculator {
     public double subtract(double a, double b) {
         return a - b;
     }
+
+    public double multiply(double a, double b) {
+        return a * b;
+    }
+
+    public double divide(double a, double b) {
+        return a / b;
+    }
 }
